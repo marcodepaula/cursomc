1d2b08b2d8cb5103aa45d93c8a2e7991fb135e4a
Marco de Paula <marcodepaula@gmail.com>
2020-08-04 17:17:59 -0300

    Estado e cidade

---
+++ src/main/java/com/marcodepaula/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.marcodepaula.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcodepaula.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
