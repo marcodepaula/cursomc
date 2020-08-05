500ecf35cb0bf413cb23982d0cb27349b400eff0
Marco de Paula <marcodepaula@gmail.com>
2020-08-05 11:05:02 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/marcodepaula/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.marcodepaula.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcodepaula.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
