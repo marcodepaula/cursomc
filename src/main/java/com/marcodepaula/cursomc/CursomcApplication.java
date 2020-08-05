ae6ab7dcf1c6adede03fa6062f2ae7a0a8b5e604
Marco de Paula <marcodepaula@gmail.com>
2020-08-05 13:41:47 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/marcodepaula/cursomc/CursomcApplication.java
+++ src/main/java/com/marcodepaula/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.marcodepaula.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.marcodepaula.cursomc.domain.Cidade;
 import com.marcodepaula.cursomc.domain.Cliente;
 import com.marcodepaula.cursomc.domain.Endereco;
 import com.marcodepaula.cursomc.domain.Estado;
+import com.marcodepaula.cursomc.domain.Pagamento;
+import com.marcodepaula.cursomc.domain.PagamentoComBoleto;
+import com.marcodepaula.cursomc.domain.PagamentoComCartao;
+import com.marcodepaula.cursomc.domain.Pedido;
 import com.marcodepaula.cursomc.domain.Produto;
+import com.marcodepaula.cursomc.domain.enums.EstadoPagamento;
 import com.marcodepaula.cursomc.domain.enums.TipoCliente;
 import com.marcodepaula.cursomc.repositories.CategoriaRepository;
 import com.marcodepaula.cursomc.repositories.CidadeRepository;
 import com.marcodepaula.cursomc.repositories.ClienteRepository;
 import com.marcodepaula.cursomc.repositories.EnderecoRepository;
 import com.marcodepaula.cursomc.repositories.EstadoRepository;
+import com.marcodepaula.cursomc.repositories.PagamentoRepository;
+import com.marcodepaula.cursomc.repositories.PedidoRepository;
 import com.marcodepaula.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -90,6 +104,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		ped1.setPagamento(pagto1);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ src/main/java/com/marcodepaula/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.marcodepaula.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcodepaula.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
